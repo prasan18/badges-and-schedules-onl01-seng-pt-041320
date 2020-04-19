ba43659b144280179de47f2e322515d6c36d1945
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-19 01:46:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,19 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end 
 def batch_badge_creator(attendees)
-"Hello, my name is #{attendees}"
-
+    badges =[]
+    attendees.each{|attendee| badges << "Hello, my name is #{attendee}." }
+    badges
 end
-def batch_badge_creator(badge)
-"Hello, my name is #{badge}."
+
+
+def assign_rooms(attendees)
+    room_assignments = []
+    attendees.each_with_index {|attendee, room| room_assignments << "Hello, #{attendee}! You'll be assigned to room #{room + 1}!"}
+    room_assignments
+end
+
+def printer(attendees)
+    badges.each {|badge| puts "#{badge}"}
+    room_assignments.each {|room| puts "#{room}"}
 end
